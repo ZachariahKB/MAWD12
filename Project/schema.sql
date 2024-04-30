7115a65354f457c65da84b1bedea213111bddfde
ZachariahKB <zachariahakb@gmail.com>
2024-04-29 18:05:55 -0700

    SQL

--- Project/schema.sql
+++ Project/schema.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS employee_table;
+CREATE DATABASE employee_table;
+\c employee_table
 -- Create department table
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
@@ -9,7 +12,10 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL REFERENCES department(id)
+    department_id INTEGER NOT NULL,
+    CONSTRAINT FK_department FOREIGN KEY (department_id)
+     REFERENCES department(id) ON DELETE CASCADE
+    
 );
 
 -- Create employee table
@@ -17,6 +23,10 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL REFERENCES role(id),
-    manager_id INTEGER REFERENCES employee(id)
+    role_id INTEGER NOT NULL, 
+    CONSTRAINT FK_role FOREIGN KEY (role_id)
+     REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER,
+    CONSTRAINT FK_manager FOREIGN KEY(manager_id)
+     REFERENCES employee(id) ON DELETE SET NULL
 );
